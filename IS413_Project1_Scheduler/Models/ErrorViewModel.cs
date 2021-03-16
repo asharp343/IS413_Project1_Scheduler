f0db6d01e7a63e906df488470be69922fc9cef0e
Anthony Sharp <asharp343@gmail.com>
2021-03-15 21:08:06 -0600

    initial

---
+++ IS413_Project1_Scheduler/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS413_Project1_Scheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
